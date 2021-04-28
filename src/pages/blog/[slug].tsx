e72a890223283c0ce71853931cbf47615fdee15e
Shinnosuke Iino <sengoku731sin@gmail.com>
2021-04-28 15:25:15 +0900

    add AD

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -69,6 +69,13 @@ const BlogPage: FC<BlogPageProps> = (props) => {
         <div className='container'>
           <div className={isPCScreen ? 'content' : 'content-mob'}>
             <DynamicBlogContent blogStringData={props.blogStringData} />
+            <br />
+            <br />
+            <div>
+            <a href="https://px.a8.net/svt/ejp?a8mat=3HA5YE+12P73M+50+2I3IZ5" rel="nofollow">
+              <img width="700" height="90" alt="" src="https://www21.a8.net/svt/bgt?aid=210426278065&wid=001&eno=01&mid=s00000000018015133000&mc=1"/></a>
+              <img width="1" height="1" src="https://www13.a8.net/0.gif?a8mat=3HA5YE+12P73M+50+2I3IZ5" alt=""/>
+            </div>
           </div>
           {isPCScreen && <div className='others'>
             <Paper elevation={10} style={{padding: '1rem'}}>
@@ -83,6 +90,11 @@ const BlogPage: FC<BlogPageProps> = (props) => {
             <br />
             <br />
             <br />
+            <div>
+              <a href="https://px.a8.net/svt/ejp?a8mat=3HA5YE+1FSQEQ+3L4M+6RHFL" rel="nofollow">
+              <img width="300" height="250" alt="" src="https://www20.a8.net/svt/bgt?aid=210426278087&wid=001&eno=01&mid=s00000016735001136000&mc=1"/></a>
+              <img width="1" height="1" src="https://www14.a8.net/0.gif?a8mat=3HA5YE+1FSQEQ+3L4M+6RHFL" alt=""/>
+            </div>
             <BlogIndex blogStringData={props.blogStringData} /> 
           </div>}
         </div>

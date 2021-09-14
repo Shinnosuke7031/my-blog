92daa9538705662bb7373036bf696808c64f4fa8
Shinnosuke Iino <sengoku731sin@gmail.com>
2021-09-14 17:26:35 +0900

    Add icon images.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,11 @@
-import Head from 'next/head'
-import React from 'react'
-import PropTypes from 'prop-types'
-import CssBaseline from '@material-ui/core/CssBaseline'
-import * as gtag from '../libs/gtag'
-import { useRouter } from 'next/router'
+import Head from "next/head";
+import React from "react";
+import PropTypes from "prop-types";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import * as gtag from "../libs/gtag";
+import { useRouter } from "next/router";
 
-import '../styles/globals.css'
+import "../styles/globals.css";
 
 export default function MyApp(props) {
   const { Component, pageProps } = props;
@@ -13,7 +13,7 @@ export default function MyApp(props) {
 
   React.useEffect(() => {
     // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector('#jss-server-side');
+    const jssStyles = document.querySelector("#jss-server-side");
     if (jssStyles) {
       jssStyles.parentElement.removeChild(jssStyles);
     }
@@ -21,24 +21,45 @@ export default function MyApp(props) {
 
   React.useEffect(() => {
     if (!gtag.existsGaId) {
-      return
+      return;
     }
 
     const handleRouteChange = (path) => {
-      gtag.pageview(path)
-    }
+      gtag.pageview(path);
+    };
 
-    router.events.on('routeChangeComplete', handleRouteChange)
+    router.events.on("routeChangeComplete", handleRouteChange);
     return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [router.events])
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
 
   return (
     <React.Fragment>
       <Head>
         <title>NOSUKE BLOG</title>
-        <link rel="icon" href="/favicon.ico" />
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}

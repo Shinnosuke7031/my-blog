cbe1ab87f6bc68d809f024008739798053eaf857
Shinnosuke Iino <sengoku731sin@gmail.com>
2021-09-16 10:53:24 +0900

    Ninja Ad

--- src/components/blog/BlogIndex.tsx
+++ src/components/blog/BlogIndex.tsx
@@ -1,78 +1,82 @@
-import { FC } from "react"
-import { Paper } from "@material-ui/core"
+import { FC } from "react";
+import { Paper } from "@material-ui/core";
+import NinjaAd from "../ad/NinjaAd";
+
 type Props = {
-  blogStringData: string
-}
+  blogStringData: string;
+};
 
 const BlogIndex: FC<Props> = (props) => {
-  const heading = props.blogStringData.match(/#+? .+/g)
-  const headingData = heading.map(el => {
-    if (el.slice(0, 2) === '# ') {
-      return {level: 1, value: el.slice(2)}
-    } else if (el.slice(0, 2) === '##') {
-      return {level: 2, value: el.slice(3)}
+  const heading = props.blogStringData.match(/#+? .+/g);
+  const headingData = heading.map((el) => {
+    if (el.slice(0, 2) === "# ") {
+      return { level: 1, value: el.slice(2) };
+    } else if (el.slice(0, 2) === "##") {
+      return { level: 2, value: el.slice(3) };
     }
-  })
+  });
   return (
-  <div style={{position: 'sticky', top: '100px'}}>
-    <Paper elevation={10}>
-      <p className='index-title'>Index</p>
-      <ul>
-        {headingData.map((data, index) => 
-          data.level === 1 ? 
-          <li key={index} className='level-h1'>
-            <a href={`#${data.value}`}>{data.value}</a>
-          </li> :
-          <li key={index}>
-            <a href={`#${data.value}`}>{data.value}</a>
-          </li>
-        )}
-      </ul>
-      <style jsx>{`
-        .index-title {
-          font-size: 1.5rem;
-          width: fit-content;
-          font-weight: bold;
-          margin: 0 0 0 1rem;
-        }
-        li {
-          font-size: 1rem;
-          list-style-type: none;
-          margin: 0 0 1rem 0;
-          padding: 0;
-        }
-        li::before {
-          position: relative;
-          top: -2px;
-          left: -16.05px;
-          display: inline-block;
-          width: 3px;
-          height: 3px;
-          content: '';
-          border-radius: 100%;
-          background: rgb(0, 0, 0);
-        }
-        .level-h1 {
-          font-weight: bold;
-        }
-        .level-h1::before {
-          width: 10px;
-          height: 10px;
-          left: -20px;
-          background: rgb(0, 137, 228);
-        }
-        .level-h1 a {
-          margin: -6px;
-        }
-      `}</style>
-    </Paper>
-    <div>
-      <a href="https://px.a8.net/svt/ejp?a8mat=3HA5YE+1FSQEQ+3L4M+6RHFL" rel="nofollow">
-      <img width="300" height="250" alt="" src="https://www20.a8.net/svt/bgt?aid=210426278087&wid=001&eno=01&mid=s00000016735001136000&mc=1"/></a>
-      <img width="1" height="1" src="https://www14.a8.net/0.gif?a8mat=3HA5YE+1FSQEQ+3L4M+6RHFL" alt=""/>
+    <div style={{ position: "sticky", top: "100px" }}>
+      <Paper elevation={10}>
+        <p className="index-title">Index</p>
+        <ul>
+          {headingData.map((data, index) =>
+            data.level === 1 ? (
+              <li key={index} className="level-h1">
+                <a href={`#${data.value}`}>{data.value}</a>
+              </li>
+            ) : (
+              <li key={index}>
+                <a href={`#${data.value}`}>{data.value}</a>
+              </li>
+            )
+          )}
+        </ul>
+        <style jsx>{`
+          .index-title {
+            font-size: 1.5rem;
+            width: fit-content;
+            font-weight: bold;
+            margin: 0 0 0 1rem;
+          }
+          li {
+            font-size: 1rem;
+            list-style-type: none;
+            margin: 0 0 1rem 0;
+            padding: 0;
+          }
+          li::before {
+            position: relative;
+            top: -2px;
+            left: -16.05px;
+            display: inline-block;
+            width: 3px;
+            height: 3px;
+            content: "";
+            border-radius: 100%;
+            background: rgb(0, 0, 0);
+          }
+          .level-h1 {
+            font-weight: bold;
+          }
+          .level-h1::before {
+            width: 10px;
+            height: 10px;
+            left: -20px;
+            background: rgb(0, 137, 228);
+          }
+          .level-h1 a {
+            margin: -6px;
+          }
+        `}</style>
+      </Paper>
+      <NinjaAd
+        id="1645c66c199fbcf4e6baf1fa953865be"
+        type="banner"
+        classAdName="admax-ads"
+      />
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default BlogIndex
+export default BlogIndex;
--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -1,60 +1,63 @@
-import { FC, Fragment } from "react"
-import { GetStaticProps } from "next"
-import Head from 'next/head'
-import dynamic from 'next/dynamic'
-import MyLayout from '../../components/MyLayout'
-import ProfileCard from '../../components/ProfileCard'
-import BlogIndex from '../../components/blog/BlogIndex'
-import fs from 'fs'
-import matter from "gray-matter"
-import { useMediaQuery } from "react-responsive"
-import styles from '../../styles/animation.module.css'
-import Paper from '@material-ui/core/Paper'
+import { FC, Fragment } from "react";
+import { GetStaticProps } from "next";
+import Head from "next/head";
+import dynamic from "next/dynamic";
+import MyLayout from "../../components/MyLayout";
+import ProfileCard from "../../components/ProfileCard";
+import BlogIndex from "../../components/blog/BlogIndex";
+import fs from "fs";
+import matter from "gray-matter";
+import { useMediaQuery } from "react-responsive";
+import styles from "../../styles/animation.module.css";
+import Paper from "@material-ui/core/Paper";
+import NinjaAd from "../../components/ad/NinjaAd";
 
-import Slider from "react-slick"
-import "slick-carousel/slick/slick.css"
-import "slick-carousel/slick/slick-theme.css"
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const DynamicBlogContent = dynamic(
-  () => import('../../components/BlogContent'),
+  () => import("../../components/BlogContent"),
   { loading: () => <div className={styles.loader}></div> }
-)
+);
 
 const DynamicSerachBlog = dynamic(
-  () => import('../../components/blog/SearchBlog'),
+  () => import("../../components/blog/SearchBlog"),
   { loading: () => <div className={styles.loader}></div> }
-)
+);
 
 type typeBlogDataArray = {
-  slug: string
-  title: string
-  description: string
-  date: string
-  imgpath: string
-  tag: string[]
-}
+  slug: string;
+  title: string;
+  description: string;
+  date: string;
+  imgpath: string;
+  tag: string[];
+};
 
 type BlogPageProps = {
-  blogStringData: string
-  title: string
-  description: string
-  allStringData: string[]
-}
+  blogStringData: string;
+  title: string;
+  description: string;
+  allStringData: string[];
+};
 
 const BlogPage: FC<BlogPageProps> = (props) => {
-  const matteredData = matter(props.blogStringData).data
-  const title = matteredData.title
-  const description = matteredData.description
-  const isPCScreen = useMediaQuery({ query: '(min-width: 1100px)'})
-  const metaData = props.allStringData.map(el => matter(el)).map(data => ({
-    slug: data.data.slug,
-    title: data.data.title,
-    description: data.data.description,
-    date: data.data.date,
-    type: data.data.type,
-    tag: data.data.tag,
-    imgpath: data.data.imgpath
-  }))
+  const matteredData = matter(props.blogStringData).data;
+  const title = matteredData.title;
+  const description = matteredData.description;
+  const isPCScreen = useMediaQuery({ query: "(min-width: 1100px)" });
+  const metaData = props.allStringData
+    .map((el) => matter(el))
+    .map((data) => ({
+      slug: data.data.slug,
+      title: data.data.title,
+      description: data.data.description,
+      date: data.data.date,
+      type: data.data.type,
+      tag: data.data.tag,
+      imgpath: data.data.imgpath,
+    }));
   const settings = {
     infinite: true,
     speed: 1000,
@@ -71,17 +74,20 @@ const BlogPage: FC<BlogPageProps> = (props) => {
         <meta name="description" content={description} />
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:site" content="@tyamatyatya" />
-        <meta property="og:url" content={`https://nosuke-blog.site/blog/${matteredData.slug}`} />
+        <meta
+          property="og:url"
+          content={`https://nosuke-blog.site/blog/${matteredData.slug}`}
+        />
         <meta property="og:title" content={title} />
-        <meta property="og:description" content={description} /> 
+        <meta property="og:description" content={description} />
         <meta property="og:image" content={matteredData.imgpath} />
       </Head>
 
       <MyLayout>
         {/* <BlogContent blogStringData={props.blogStringData} title={props.title} description={props.description} /> */}
-        <div className='container'>
-          <div className={isPCScreen ? 'content' : 'content-mob'}>
-            <div className="top_ad" style={{position: 'sticky', top: '80px'}}>
+        <div className="container">
+          <div className={isPCScreen ? "content" : "content-mob"}>
+            {/* <div className="top_ad" style={{position: 'sticky', top: '80px'}}>
               <Slider {...settings} className="slick">
                 <div>
                   <a href="https://px.a8.net/svt/ejp?a8mat=3HA5YE+1FSQEQ+3L4M+6GZCI" rel="nofollow">フロントエンドエンジニアになりたい人の Webプログラミング入門</a>
@@ -96,46 +102,83 @@ const BlogPage: FC<BlogPageProps> = (props) => {
                   <img width="1" height="1" src="https://www12.a8.net/0.gif?a8mat=3HA5YE+1FSQEQ+3L4M+609HU" alt="" />
                 </div>
               </Slider>
+            </div> */}
+            <div className="ninja">
+              <NinjaAd
+                width={700}
+                height={90}
+                id="5e823f019e8a4622326ff899607b271f"
+                type="switch"
+                classAdName="admax-switch"
+              />
             </div>
             <br />
             <DynamicBlogContent blogStringData={props.blogStringData} />
             <br />
             <br />
             <div className="banner-ad">
-              <a href="https://px.a8.net/svt/ejp?a8mat=3HA5YE+12P73M+50+2I3IZ5" rel="nofollow">
-                <img width="90%" alt="" src="https://www21.a8.net/svt/bgt?aid=210426278065&wid=001&eno=01&mid=s00000000018015133000&mc=1"/>
+              <a
+                href="https://px.a8.net/svt/ejp?a8mat=3HA5YE+12P73M+50+2I3IZ5"
+                rel="nofollow"
+              >
+                <img
+                  width="90%"
+                  alt=""
+                  src="https://www21.a8.net/svt/bgt?aid=210426278065&wid=001&eno=01&mid=s00000000018015133000&mc=1"
+                />
               </a>
-              <img width="1" height="1" src="https://www13.a8.net/0.gif?a8mat=3HA5YE+12P73M+50+2I3IZ5" alt=""/>
+              <img
+                width="1"
+                height="1"
+                src="https://www13.a8.net/0.gif?a8mat=3HA5YE+12P73M+50+2I3IZ5"
+                alt=""
+              />
             </div>
           </div>
-          {isPCScreen && <div className='others'>
-            <Paper elevation={10} style={{padding: '1rem'}}>
-              <DynamicSerachBlog isTagDisplayed={false} blogMetaData={metaData} />
-            </Paper>
-            <br />
-            <br />
-            <ProfileCard />
-            <br />
-            <br />
-            <BlogIndex blogStringData={props.blogStringData} /> 
-            <br />
-            <br />
-            {/* <div>
+          {isPCScreen && (
+            <div className="others">
+              <Paper elevation={10} style={{ padding: "1rem" }}>
+                <DynamicSerachBlog
+                  isTagDisplayed={false}
+                  blogMetaData={metaData}
+                />
+              </Paper>
+              <br />
+              <br />
+              <ProfileCard />
+              <br />
+              <br />
+              <BlogIndex blogStringData={props.blogStringData} />
+              <br />
+              <br />
+              {/* <div>
               <a href="https://px.a8.net/svt/ejp?a8mat=3HA5YE+1FSQEQ+3L4M+6RHFL" rel="nofollow">
               <img width="300" height="250" alt="" src="https://www20.a8.net/svt/bgt?aid=210426278087&wid=001&eno=01&mid=s00000016735001136000&mc=1"/></a>
               <img width="1" height="1" src="https://www14.a8.net/0.gif?a8mat=3HA5YE+1FSQEQ+3L4M+6RHFL" alt=""/>
             </div> */}
-          </div>}
+            </div>
+          )}
         </div>
+        <NinjaAd
+          id="6f5bc9be8374105339be181245668f6f"
+          type="overlay"
+          classAdName="admax-ads"
+        />
       </MyLayout>
       <style jsx>{`
+        .ninja {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+        }
         .top_ad {
           width: 100%;
           background-color: #ffffff;
           border-top: rgb(153, 153, 153) 1px solid;
           border-bottom: rgb(153, 153, 153) 1.5px solid;
           border-radius: 2px;
-          box-shadow: 0px 3px 3px -2px rgb(0 0 0 / 20%), 0px 3px 4px 0px rgb(0 0 0 / 14%), 0px 1px 8px 0px rgb(0 0 0 / 12%);
+          box-shadow: 0px 3px 3px -2px rgb(0 0 0 / 20%),
+            0px 3px 4px 0px rgb(0 0 0 / 14%), 0px 1px 8px 0px rgb(0 0 0 / 12%);
         }
         .top_ad div a {
           display: inline-block;
@@ -174,49 +217,51 @@ const BlogPage: FC<BlogPageProps> = (props) => {
         }
       `}</style>
     </Fragment>
-  )
-}
+  );
+};
 
 export async function getStaticPaths() {
-  const files = fs.readdirSync(process.cwd() + '/docs', 'utf8')
-  const blogs = files.filter((fn) => fn.endsWith(".md"))
+  const files = fs.readdirSync(process.cwd() + "/docs", "utf8");
+  const blogs = files.filter((fn) => fn.endsWith(".md"));
   const blogStringDataArr = blogs.map((blog) => {
-    const path = `${process.cwd()}/docs/${blog}`
+    const path = `${process.cwd()}/docs/${blog}`;
     const data = fs.readFileSync(path, {
       encoding: "utf-8",
-    })
-    return data
-  })
-  const matteredData = blogStringDataArr.map(blog => matter(blog))
-  const blogDataArray = matteredData.map(el => el.data)
+    });
+    return data;
+  });
+  const matteredData = blogStringDataArr.map((blog) => matter(blog));
+  const blogDataArray = matteredData.map((el) => el.data);
   const paths = blogDataArray.map((path: typeBlogDataArray) => ({
     params: { slug: path.slug },
-  }))
+  }));
 
-  return { paths, fallback: false }
+  return { paths, fallback: false };
 }
 
-export const getStaticProps: GetStaticProps = async ({params}) => {
-  const file = fs.readFileSync(`${process.cwd()}/docs/${params.slug}.md`, 'utf8')
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const file = fs.readFileSync(
+    `${process.cwd()}/docs/${params.slug}.md`,
+    "utf8"
+  );
   const siteData = await import(`../../../config.json`);
-  const allFiles = fs.readdirSync(process.cwd() + '/docs', 'utf8')
-  const blogs = allFiles.filter((fn) => fn.endsWith(".md"))
+  const allFiles = fs.readdirSync(process.cwd() + "/docs", "utf8");
+  const blogs = allFiles.filter((fn) => fn.endsWith(".md"));
   const blogStringDataArr = blogs.map((blog) => {
-    const path = `${process.cwd()}/docs/${blog}`
+    const path = `${process.cwd()}/docs/${blog}`;
     const data = fs.readFileSync(path, {
       encoding: "utf-8",
-    })
-    return data
-  })
+    });
+    return data;
+  });
 
   return {
-    props : {
+    props: {
       blogStringData: file,
       title: siteData.default.title,
       description: siteData.default.description,
-      allStringData: blogStringDataArr
-    }
-  }
-
-}
-export default BlogPage
+      allStringData: blogStringDataArr,
+    },
+  };
+};
+export default BlogPage;
